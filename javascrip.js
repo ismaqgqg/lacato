5a5def6738d9226ad3f2bd5ce0e43536ba130322
2313546549876435464hfashvkjsbjbjbjbasljbcjb <167261802+ismaqgqg@users.noreply.github.com>
2024-08-29 11:24:55 -0500

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const navMenu = document.querySelector('.nav ul');
+    navMenu.classList.toggle('show'); // Alterna la clase "show" para mostrar u ocultar el menú
+  });
+  
